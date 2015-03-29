7a4b93719f3dc68cc917e6134023b8a4b4c86f0b
vips2010 <vipul.a.save@gmail.com>
2015-03-29 19:36:08 -0400

    Added Three Views

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
 });
+
+/* GET about me page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About Me' });
+});
+
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact Me' });
+});
+
 
 module.exports = router;
